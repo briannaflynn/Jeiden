1304567bd61359bf3e5524a64788d5b3a64572a2
Brie Flynn <65606659+briannaflynn@users.noreply.github.com>
2024-03-07 06:19:56 -0600

    aggregation method with JAX inverse indexing and matrix multiplication

--- aggregate.py
+++ aggregate.py
@@ -4,11 +4,14 @@ def aggregate_nodes(adj_matrix, communities):
 
   # first get the number of unique communities and new community assignments
   # new community assignments correspond to the index of each unique community in the aggregated graph - index the 'super-nodes'
-
-  # next create a community matrix where rows are nodes and columns are communitiies
-
+  unique_communities, inverse_indices = jnp.unique(communities, return_inverse = True)
+  num_communities = len(unique_communities)
+  new_communities = jnp.arange(num_communities)
+  
+  # initialize matrix with zeros then fill matrix where rows are nodes and columns are communitiies
+  community_matrix = jnp.zeros((len(communities), num_communities)).at[jnp.arange(len(communities)), inverse_indices].set(1)
+  
   # aggregate the adjacency matrix by multiplying the transpose of the community matrix to the adjacency matrix and again with the original community matrix
+  aggregated_adj_matrix = community_matrix.T @ adj_matrix @ community_matrix
 
-
-
-  # return aggregated_adj_matrix, new_communities
+  return aggregated_adj_matrix, new_communities
